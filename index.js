5e834f676e4c3a4f67c9755c8aedc1140d07975c
Leonardo Véliz <leodexe@gmail.com>
2022-11-03 10:58:16 -0500

    change to public dir

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 const server = express();
 import cors from "cors";
 server.use(cors());
-server.use(express.static('burrito'));
+server.use(express.static('public'));
 server.use(express.json());
 const playerIDs = [];
 let playerDB = [];
